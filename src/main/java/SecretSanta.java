7b260e31dad172c4371bfe859385d47c3a2650f5
Alex Kröker <akr@Alexs-MacBook-Pro.local>
2015-12-08 10:33:00 +0100

    init

---
+++ src/main/java/SecretSanta.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+/**
+ * Created by akr on 08.12.15.
+ */
+public class SecretSanta {
+
+    private List<String> participants;
+
+    public SecretSanta(final List<String> participants) {
+        this.participants = new ArrayList<String>(participants.size());
+        for(String participant : participants) {
+            this.participants.add(participant);
+        }
+    }
+
+    public SecretSanta(String... participants) {
+        this.participants = new ArrayList<String>(Arrays.asList(participants));
+    }
+
+    public void ziehen(final String participant) {
+        Random random = new Random();
+
+        String user;
+        do {
+            int index = random.nextInt(participants.size());
+            user = participants.get(index);
+        } while (user.equals(participant));
+
+        System.out.println(user + " hat " + participant + " gezogen! ;D");
+        participants.remove(user);
+    }
+
+    public static void main(String[] args) {
+        List<String> participants = Arrays.asList("Alex", "Torben", "Sandra", "Jenny", "Tim", "Jens", "Marcus");
+        SecretSanta santa = new SecretSanta(participants);
+
+        for(String participant : participants) {
+            santa.ziehen(participant);
+        }
+    }
+
+}
